1325dd7365995141b1a23e0ed7470283e9939bd5
Xi59264-GirishAdapa <59917556+Xi59264-GirishAdapa@users.noreply.github.com>
2021-01-13 22:44:53 +0530

    Initial Commit

---
+++ ChatAppUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatAppUI
+//
+//  Created by Dnyaneshwar on 13/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
